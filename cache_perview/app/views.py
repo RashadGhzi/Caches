7154f77efd309d34610ad43006cff6253fe60d26
Rashad <rashadarsh0@gmail.com>
2023-02-16 21:31:31 +0600

    First commit

---
+++ cache_perview/app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+# Create your views here.
+
+
+@cache_page(60)
+def home(request):
+    return render(request, 'app/home.html')
